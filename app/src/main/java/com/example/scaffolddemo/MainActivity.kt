472592f83eec240ab96a34a6aa46d268a7a9f162
Aiden Chang <asdf62210@gmail.com>
2023-12-05 19:41:29 +0800

    Zhongshan Home - v1 Complete

--- app/src/main/java/com/example/scaffolddemo/MainActivity.kt
+++ app/src/main/java/com/example/scaffolddemo/MainActivity.kt
@@ -3,13 +3,7 @@ package com.example.scaffolddemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.scaffolddemo.ui.screens.ScaffoldDemo
 import com.example.scaffolddemo.ui.theme.ScaffoldDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +11,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ScaffoldDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                ScaffoldDemo()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ScaffoldDemoTheme {
-        Greeting("Android")
     }
 }
